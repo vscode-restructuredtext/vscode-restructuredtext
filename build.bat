69bdd3e47fbd3ae8373cadd19dd2adcd31e51875
Lex Li <support@lextm.com>
2018-09-02 10:04:25 -0400

    Moved package command.

--- build.bat
+++ build.bat
@@ -2,7 +2,3 @@ echo to install dependencies
 npm install
 @IF %ERRORLEVEL% NEQ 0 EXIT /b 1
 echo installed dependencies
-echo to generate package
-vsce package
-@IF %ERRORLEVEL% NEQ 0 EXIT /b 1
-echo generated package
