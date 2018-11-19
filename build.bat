eb39624fa09718c6efb0fd998fdecaee303d95a4
Lex Li <support@lextm.com>
2018-11-19 12:15:48 -0500

    Enabled test cases.

--- build.bat
+++ build.bat
@@ -2,3 +2,8 @@ echo to install dependencies
 npm install
 @IF %ERRORLEVEL% NEQ 0 EXIT /b 1
 echo installed dependencies
+
+echo to run test cases
+npm run test
+@IF %ERRORLEVEL% NEQ 0 EXIT /b 1
+echo ran test cases
