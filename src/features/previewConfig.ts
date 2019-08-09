vscode-restructuredtext/vscode-restructuredtext

commit 74cb99b91a71b3e3638e81676c6219934f4f1360

Switched to RST-vscode approach.

src/features/previewConfig.ts
@@ -1,0 +1,111 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import { join } from 'path';
+
+export class RSTPreviewConfiguration {
+	public static getForResource(resource: vscode.Uri) {
+		return new RSTPreviewConfiguration(resource);
+	}
+
+	public readonly scrollBeyondLastLine: boolean;
+	public readonly wordWrap: boolean;
+	public readonly doubleClickToSwitchToEditor: boolean;
+	public readonly scrollEditorWithPreview: boolean;
+	public readonly scrollPreviewWithEditor: boolean;
+	public readonly rstEditorSelection: boolean;
+
+	public readonly lineHeight: number;
+	public readonly fontSize: number;
+	public readonly fontFamily: string | undefined;
+	public readonly styles: string[];
+	public readonly baseStyles: string[];
+
+	private constructor(resource: vscode.Uri) {
+		const editorConfig = vscode.workspace.getConfiguration('editor', resource);
+		const rstConfig = vscode.workspace.getConfiguration('rst', resource);
+		const rstEditorConfig = vscode.workspace.getConfiguration('[rst]', resource);
+
+
+		this.scrollBeyondLastLine = editorConfig.get<boolean>('scrollBeyondLastLine', false);
+
+		this.wordWrap = editorConfig.get<string>('wordWrap', 'off') !== 'off';
+		if (rstEditorConfig && rstEditorConfig['editor.wordWrap']) {
+			this.wordWrap = rstEditorConfig['editor.wordWrap'] !== 'off';
+		}
+
+		this.scrollPreviewWithEditor = !!rstConfig.get<boolean>('preview.scrollPreviewWithEditor', true);
+		this.scrollEditorWithPreview = !!rstConfig.get<boolean>('preview.scrollEditorWithPreview', true);
+		this.lineBreaks = !!rstConfig.get<boolean>('preview.breaks', false);
+		this.doubleClickToSwitchToEditor = !!rstConfig.get<boolean>('preview.doubleClickToSwitchToEditor', true);
+		this.rstEditorSelection = !!rstConfig.get<boolean>('preview.markEditorSelection', true);
+		this.pythonPath = rstConfig.get<string>("preview.pythonPath", "python");
+
+		this.fontFamily = rstConfig.get<string | undefined>('preview.fontFamily', undefined);
+		this.fontSize = Math.max(8, +rstConfig.get<number>('preview.fontSize', NaN));
+		this.lineHeight = Math.max(0.6, +rstConfig.get<number>('preview.lineHeight', NaN));
+
+		this.baseStyles = [
+			join(__dirname, "..", "..", "media", "basic.css"),
+			join(__dirname, "..", "..", "media", "default.css"),
+			join(__dirname, "..", "..", "media", "rst.css")
+		];
+
+		this.styles = rstConfig.get<string[]>('styles', []);
+	}
+
+	public isEqualTo(otherConfig: RSTPreviewConfiguration) {
+		for (let key in this) {
+			if (this.hasOwnProperty(key) && key !== 'styles') {
+				if (this[key] !== otherConfig[key]) {
+					return false;
+				}
+			}
+		}
+
+		// Check styles
+		if (this.styles.length !== otherConfig.styles.length) {
+			return false;
+		}
+		for (let i = 0; i < this.styles.length; ++i) {
+			if (this.styles[i] !== otherConfig.styles[i]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	[key: string]: any;
+}
+
+export class RSTPreviewConfigurationManager {
+	private readonly previewConfigurationsForWorkspaces = new Map<string, RSTPreviewConfiguration>();
+
+	public loadAndCacheConfiguration(
+		resource: vscode.Uri
+	): RSTPreviewConfiguration {
+		const config = RSTPreviewConfiguration.getForResource(resource);
+		this.previewConfigurationsForWorkspaces.set(this.getKey(resource), config);
+		return config;
+	}
+
+	public hasConfigurationChanged(
+		resource: vscode.Uri
+	): boolean {
+		const key = this.getKey(resource);
+		const currentConfig = this.previewConfigurationsForWorkspaces.get(key);
+		const newConfig = RSTPreviewConfiguration.getForResource(resource);
+		return (!currentConfig || !currentConfig.isEqualTo(newConfig));
+	}
+
+	private getKey(
+		resource: vscode.Uri
+	): string {
+		const folder = vscode.workspace.getWorkspaceFolder(resource);
+		return folder ? folder.uri.toString() : '';
+	}
+}

preview-src/activeLineMarker.ts
@@ -1,0 +1,34 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { getElementsForSourceLine } from './scroll-sync';
+
+export class ActiveLineMarker {
+	private _current: any;
+
+	onDidChangeTextEditorSelection(line: number) {
+		const { previous } = getElementsForSourceLine(line);
+		this._update(previous && previous.element);
+	}
+
+	_update(before: HTMLElement | undefined) {
+		this._unmarkActiveElement(this._current);
+		this._markActiveElement(before);
+		this._current = before;
+	}
+
+	_unmarkActiveElement(element: HTMLElement | undefined) {
+		if (!element) {
+			return;
+		}
+		element.className = element.className.replace(/\bcode-active-line\b/g, '');
+	}
+
+	_markActiveElement(element: HTMLElement | undefined) {
+		if (!element) {
+			return;
+		}
+		element.className += ' code-active-line';
+	}
+}

preview-src/csp.ts
@@ -1,0 +1,64 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { MessagePoster } from './messaging';
+import { getSettings } from './settings';
+import { getStrings } from './strings';
+
+/**
+ * Shows an alert when there is a content security policy violation.
+ */
+export class CspAlerter {
+	private didShow = false;
+	private didHaveCspWarning = false;
+
+	private messaging?: MessagePoster;
+
+	constructor() {
+		document.addEventListener('securitypolicyviolation', () => {
+			this.onCspWarning();
+		});
+
+		window.addEventListener('message', (event) => {
+			if (event && event.data && event.data.name === 'vscode-did-block-svg') {
+				this.onCspWarning();
+			}
+		});
+	}
+
+	public setPoster(poster: MessagePoster) {
+		this.messaging = poster;
+		if (this.didHaveCspWarning) {
+			this.showCspWarning();
+		}
+	}
+
+	private onCspWarning() {
+		this.didHaveCspWarning = true;
+		this.showCspWarning();
+	}
+
+	private showCspWarning() {
+		const strings = getStrings();
+		const settings = getSettings();
+
+		if (this.didShow || settings.disableSecurityWarnings || !this.messaging) {
+			return;
+		}
+		this.didShow = true;
+
+		const notification = document.createElement('a');
+		notification.innerText = strings.cspAlertMessageText;
+		notification.setAttribute('id', 'code-csp-warning');
+		notification.setAttribute('title', strings.cspAlertMessageTitle);
+
+		notification.setAttribute('role', 'button');
+		notification.setAttribute('aria-label', strings.cspAlertMessageLabel);
+		notification.onclick = () => {
+			this.messaging!.postCommand('rst.showPreviewSecuritySelector', [settings.source]);
+		};
+		document.body.appendChild(notification);
+	}
+}

preview-src/events.ts
@@ -1,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export function onceDocumentLoaded(f: () => void) {
+	if (document.readyState === 'loading') {
+		document.addEventListener('DOMContentLoaded', f);
+	} else {
+		f();
+	}
+}

preview-src/index.ts
@@ -1,0 +1,129 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ActiveLineMarker } from './activeLineMarker';
+import { onceDocumentLoaded } from './events';
+import { createPosterForVsCode } from './messaging';
+import { getEditorLineNumberForPageOffset, scrollToRevealSourceLine } from './scroll-sync';
+import { getSettings, getData } from './settings';
+import throttle = require('lodash.throttle');
+
+declare var acquireVsCodeApi: any;
+
+var scrollDisabled = true;
+const marker = new ActiveLineMarker();
+const settings = getSettings();
+
+const vscode = acquireVsCodeApi();
+
+// Set VS Code state
+const state = getData('data-state');
+vscode.setState(state);
+
+const messaging = createPosterForVsCode(vscode);
+
+window.cspAlerter.setPoster(messaging);
+
+onceDocumentLoaded(() => {
+	if (settings.scrollPreviewWithEditor) {
+		setTimeout(() => {
+			const initialLine = +settings.line;
+			if (!isNaN(initialLine)) {
+				scrollDisabled = true;
+				scrollToRevealSourceLine(initialLine);
+			}
+		}, 0);
+	}
+});
+
+const onUpdateView = (() => {
+	const doScroll = throttle((line: number) => {
+		scrollDisabled = true;
+		scrollToRevealSourceLine(line);
+	}, 50);
+
+	return (line: number, settings: any) => {
+		if (!isNaN(line)) {
+			settings.line = line;
+			doScroll(line);
+		}
+	};
+})();
+
+window.addEventListener('resize', () => {
+	scrollDisabled = true;
+}, true);
+
+window.addEventListener('message', event => {
+	if (event.data.source !== settings.source) {
+		return;
+	}
+
+	switch (event.data.type) {
+		case 'onDidChangeTextEditorSelection':
+			marker.onDidChangeTextEditorSelection(event.data.line);
+			break;
+
+		case 'updateView':
+			onUpdateView(event.data.line, settings);
+			break;
+	}
+}, false);
+
+document.addEventListener('dblclick', event => {
+	if (!settings.doubleClickToSwitchToEditor) {
+		return;
+	}
+
+	// Ignore clicks on links
+	for (let node = event.target as HTMLElement; node; node = node.parentNode as HTMLElement) {
+		if (node.tagName === 'A') {
+			return;
+		}
+	}
+
+	const offset = event.pageY;
+	const line = getEditorLineNumberForPageOffset(offset);
+	if (typeof line === 'number' && !isNaN(line)) {
+		messaging.postMessage('didClick', { line: Math.floor(line) });
+	}
+});
+
+document.addEventListener('click', event => {
+	if (!event) {
+		return;
+	}
+
+	let node: any = event.target;
+	while (node) {
+		if (node.tagName && node.tagName === 'A' && node.href) {
+			if (node.getAttribute('href').startsWith('#')) {
+				break;
+			}
+			if (node.href.startsWith('file://') || node.href.startsWith('vscode-resource:')) {
+				const [path, fragment] = node.href.replace(/^(file:\/\/|vscode-resource:)/i, '').split('#');
+				messaging.postCommand('_rst.openDocumentLink', [{ path, fragment }]);
+				event.preventDefault();
+				event.stopPropagation();
+				break;
+			}
+			break;
+		}
+		node = node.parentNode;
+	}
+}, true);
+
+if (settings.scrollEditorWithPreview) {
+	window.addEventListener('scroll', throttle(() => {
+		if (scrollDisabled) {
+			scrollDisabled = false;
+		} else {
+			const line = getEditorLineNumberForPageOffset(window.scrollY);
+			if (typeof line === 'number' && !isNaN(line)) {
+				messaging.postMessage('revealLine', { line });
+			}
+		}
+	}, 50));
+}

preview-src/messaging.ts
@@ -1,0 +1,35 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { getSettings } from './settings';
+
+export interface MessagePoster {
+	/**
+	 * Post a message to the rst extension
+	 */
+	postMessage(type: string, body: object): void;
+
+
+	/**
+	 * Post a command to be executed to the rst extension
+	 */
+	postCommand(command: string, args: any[]): void;
+}
+
+export const createPosterForVsCode = (vscode: any) => {
+	return new class implements MessagePoster {
+		postMessage(type: string, body: object): void {
+			vscode.postMessage({
+				type,
+				source: getSettings().source,
+				body
+			});
+		}
+		postCommand(command: string, args: any[]) {
+			this.postMessage('command', { command, args });
+		}
+	};
+};
+

commit faa92e0125adaac6336f0de4cb9bdc4398319d26

Add missing lineBreaks property
Typescript should've been failing since https://github.com/vscode-restructuredtext/vscode-restructuredtext/commit/74cb99b91a71b3e3638e81676c6219934f4f1360 because of this? Have the errors been suppressed somehow?

src/features/previewConfig.ts
@@ -14,6 +14,7 @@ export class RSTPreviewConfiguration {
 	public readonly scrollBeyondLastLine: boolean;
 	public readonly wordWrap: boolean;
 	public readonly doubleClickToSwitchToEditor: boolean;
+	public readonly lineBreaks: boolean;
 	public readonly scrollEditorWithPreview: boolean;
 	public readonly scrollPreviewWithEditor: boolean;
 	public readonly rstEditorSelection: boolean;