999b277b96028e734a024737e2521480b2f28438
Lex Li <support@lextm.com>
2020-12-08 01:43:04 -0500

    Initial work to add snooty.

---
+++ src/rstLsp/docLinkProvider.ts
@@ -0,0 +1,96 @@
+import * as vscode from "vscode";
+import { LanguageClient, CancellationToken } from "vscode-languageclient";
+
+export class DocumentLinkProvider implements vscode.DocumentLinkProvider {
+  /**
+   * Handles creation of document links (https://code.visualstudio.com/api/references/vscode-api#DocumentLink)
+   * for doc roles. Clicking on the target of a document link allows the user to open a text editor with its
+   * corresponding file.
+   */
+
+  private _client: LanguageClient;
+
+  constructor(client: LanguageClient) {
+    this._client = client;
+  }
+
+  // Provides the text document with the ranges for document links
+  provideDocumentLinks(
+    document: vscode.TextDocument,
+    token: CancellationToken
+  ): vscode.ProviderResult<vscode.DocumentLink[]> {
+    return this._findDocLinks(document);
+  }
+
+  // Adds the target uri to the document link
+  async resolveDocumentLink(
+    link: vscode.DocumentLink,
+    token: CancellationToken
+  ): Promise<vscode.DocumentLink | undefined> {
+    const activeTextEditor = vscode.window.activeTextEditor;
+    if (!activeTextEditor) {
+      return undefined;
+    }
+    const document = activeTextEditor.document;
+    const text = document.getText(link.range);
+    link.target = await this._findTargetUri(document, text);
+    return link;
+  }
+
+  // Returns document links found within the current text document
+  private _findDocLinks(document: vscode.TextDocument): vscode.DocumentLink[] {
+    const docText = document.getText();
+    const docRoles = docText.match(/:doc:`.+?`/gs);
+
+    if (docRoles === null) return [];
+
+    let doclinks: vscode.DocumentLink[] = [];
+    let docRoleOffsetStart = -1; // Initiated to -1 to accommodate 0th index
+
+    // For every doc role found, find their respective target
+    for (const docRole of docRoles) {
+      docRoleOffsetStart = docText.indexOf(docRole, docRoleOffsetStart + 1);
+
+      // Find target in doc role
+      // Check if target exists in the form :doc:`text <target-name>`
+      let targetMatches = docRole.match(/(?<=<)\S+(?=>)/);
+      // If target not found, target should exist in the form :doc:`target-name`
+      if (targetMatches === null) {
+        targetMatches = docRole.match(/(?<=`)\S+(?=`)/);
+      }
+      if (!targetMatches) {
+        continue;
+      }
+      const target = targetMatches[0];
+      const targetIndex = docRole.indexOf(target);
+
+      // Get range of the target within the scope of the whole text document
+      const targetOffsetStart = docRoleOffsetStart + targetIndex;
+      const targetOffsetEnd = targetOffsetStart + target.length;
+
+      doclinks.push({
+        range: new vscode.Range(
+          document.positionAt(targetOffsetStart),
+          document.positionAt(targetOffsetEnd)
+        )
+      });
+    }
+
+    return doclinks;
+  }
+
+  // Returns the full uri given a target's name
+  private async _findTargetUri(
+    document: vscode.TextDocument,
+    target: string
+  ): Promise<vscode.Uri> {
+    const file: string = await this._client
+      .sendRequest("textDocument/resolve", {
+        fileName: target,
+        docPath: document.uri.path,
+        resolveType: "doc"
+      })
+
+    return vscode.Uri.file(file);
+  }
+}
--- src/rstLsp/extension.ts
+++ src/rstLsp/extension.ts
@@ -7,6 +7,9 @@ import * as vscode from 'vscode';
 import { LanguageClient, ServerOptions, LanguageClientOptions } from 'vscode-languageclient';
 import * as path from "path";
 import { Logger } from '../logger';
+import { DocumentLinkProvider } from './docLinkProvider';
+import open = require('open');
+import mime = require('mime');
 
 export function activate(context: vscode.ExtensionContext, logger: Logger, disabled: boolean) {
     if (disabled) {
@@ -15,6 +18,7 @@ export function activate(context: vscode.ExtensionContext, logger: Logger, disab
 
     // Defines the search path of your language server DLL. (.NET Core)
     const languageServerPaths = [
+        ".snooty/snooty/snooty",
         "../restructuredtext-antlr/Server/bin/Debug/netcoreapp3.1/Server.dll",
         ".rst/Server.exe",
         ".rst/Server"
@@ -37,8 +41,8 @@ export function activate(context: vscode.ExtensionContext, logger: Logger, disab
         // If the extension is launched in debug mode then the debug server options are used
         // Otherwise the run options are used
         let serverOptions: ServerOptions = {
-            run: { command: serverModule, args: [], options: { cwd: workPath } },
-            debug: { command: serverModule, args: ["--debug"], options: { cwd: workPath } }
+            run: { command: serverModule, args: ["language-server"], options: { cwd: workPath } },
+            debug: { command: serverModule, args: ["language-server"], options: { cwd: workPath } }
         }
 
         if (serverModule.indexOf(".dll") > -1)
@@ -49,30 +53,103 @@ export function activate(context: vscode.ExtensionContext, logger: Logger, disab
             }
         }
 
+        const documentSelector = [
+            { language: 'plaintext', scheme: 'file' },
+            { language: 'yaml', scheme: 'file' },
+            { language: 'restructuredtext', scheme: 'file' },
+            { language: 'toml', scheme: 'file' },
+        ];
         // Options to control the language client
         let clientOptions: LanguageClientOptions = {
             // Register the server for plain text documents
-            documentSelector: [
-                { language: 'restructuredtext', scheme: 'file' },
-                { language: 'restructuredtext', scheme: 'untitled' }
-            ],
+            documentSelector,
             synchronize: {
                 // Synchronize the setting section 'lspSample' to the server
-                configurationSection: 'restructuredtext',
+                configurationSection: 'snooty',
                 // Notify the server about file changes to '.clientrc' files contain in the workspace
                 fileEvents: [
-                    vscode.workspace.createFileSystemWatcher('**/conf.py'),
-                    vscode.workspace.createFileSystemWatcher("**/.rst"),
-                    vscode.workspace.createFileSystemWatcher("**/.rest")
+                    vscode.workspace.createFileSystemWatcher('**/*.rst'),
+                    vscode.workspace.createFileSystemWatcher('**/*.txt'),
+                    vscode.workspace.createFileSystemWatcher('**/*.yaml'),
+                    vscode.workspace.createFileSystemWatcher('snooty.toml')
                 ]
             }
         }
 
-        // Create the language client and start the client.
-        let disposable = new LanguageClient('restructuredtext', 'reStructuredText Language Server', serverOptions, clientOptions).start();
-
+        const client = new LanguageClient('Snooty Language Client', serverOptions, clientOptions);
+        const restartServer = vscode.commands.registerCommand('snooty.restart', async () => {
+            await client.stop();
+            return client.start();
+        });
+    
         // Push the disposable to the context's subscriptions so that the
         // client can be deactivated on extension deactivation
-        context.subscriptions.push(disposable);  
+        context.subscriptions.push(client.start());
+        context.subscriptions.push(restartServer);
+    
+        // Register custom command to allow includes, literalincludes, and figures to be clickable
+        let hoverFile: string;
+        const clickInclude: vscode.Disposable = vscode.commands.registerCommand('snooty.clickInclude', async () => {
+            // Send request to server (snooty-parser)
+            const type = mime.getType(hoverFile);
+    
+            if (type == null || !type.includes("image")) {
+                const textDoc = await vscode.workspace.openTextDocument(hoverFile);
+                vscode.window.showTextDocument(textDoc);
+            }
+            else {
+                open(hoverFile);
+            }
+        });
+        context.subscriptions.push(clickInclude);
+    
+        // Shows clickable link to file after hovering over it
+        vscode.languages.registerHoverProvider(
+            documentSelector,
+            new class implements vscode.HoverProvider {
+              provideHover(
+                _document: vscode.TextDocument,
+                _position: vscode.Position,
+                _token: vscode.CancellationToken
+              ): vscode.ProviderResult<vscode.Hover> {
+                // Get range for a link
+                const wordRegex = /\/\S+/;
+                const wordRange = _document.getWordRangeAtPosition(_position, wordRegex);
+    
+                if (wordRange != undefined) {
+                    // Get text at that range
+                    const word = _document.getText(wordRange);
+    
+                    // Request hover information using the snooty-parser server
+                    let request = async () => {
+                        let contents: vscode.MarkdownString;
+    
+                        const file: string = await client.sendRequest("textDocument/resolve", {fileName: word, docPath: _document.uri.path, resolveType: "directive"});
+                        const command = vscode.Uri.parse(`command:snooty.clickInclude`);
+    
+                        // Clean up absolute path for better UX. str.match() was not working with regex but can look into later
+                        let workspaceFolder = vscode.workspace.name;
+                        if (!workspaceFolder) {
+                            return;
+                        }
+                        let folderIndex = file.search(workspaceFolder);
+                        let hoverPathRelative = file.slice(folderIndex);
+    
+                        contents = new vscode.MarkdownString(`[${hoverPathRelative}](${command})`);
+                        contents.isTrusted = true; // Enables commands to be used
+    
+                        return new vscode.Hover(contents, wordRange);
+                    }
+    
+                    return request();
+                }
+              }
+            } ()
+        );
+    
+        vscode.languages.registerDocumentLinkProvider(
+            documentSelector,
+            new DocumentLinkProvider(client)
+        );
     }
 }
