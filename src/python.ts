27d85cfbe61eb6827fcb764110c2de8ff02e68e5
Lex Li <support@lextm.com>
2021-02-06 15:15:06 -0500

    Bumped Snooty version.

--- src/python.ts
+++ src/python.ts
@@ -367,7 +367,7 @@ export class Python {
 
   private async checkSnootyInstall(): Promise<boolean> {
     try {
-      const versionTooOld = await this.exec("-c", '"import snooty; from distutils.version import LooseVersion; print(LooseVersion(snooty.__version__) < LooseVersion(\'1.8.6\'))"');
+      const versionTooOld = await this.exec("-c", '"import snooty; from distutils.version import LooseVersion; print(LooseVersion(snooty.__version__) < LooseVersion(\'1.8.7\'))"');
       return versionTooOld.trim() === 'False';
     } catch (e) {
       return false;
